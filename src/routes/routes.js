a261f33c76da9adf047402fb2eac6c854875d263
Bharat <bharat@sateeq.com>
2023-08-29 00:18:51 +0530

    testing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,8 @@ import foodRouter from "./food.router.js";
 const routes = Router();
 
 routes.use("/food", foodRouter);
+routes.get('/',(req,res)=>{
+    res.send('Working');
+})
 
 export default routes;
